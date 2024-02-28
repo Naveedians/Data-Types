d815a300ce7f5935b1be09a1bc21da7317d16f3e
Naveed Ahmed <160510766+Naveedians@users.noreply.github.com>
2024-02-28 10:10:18 +0500

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+//Data Types
+//String
+let batsmanName = "AB Devilliers"; //This is the name of batsmanName
+console.log(batsmanName); // print the batsmanName
+//Number
+let score = 200; //this is the score of batsman
+console.log(score); // print batsman score
+//Boolean
+let isout = true; //this is the boolean value
+console.log(isout); // print the boolean value
+//Null
+let data = null; //in this code data be null
+console.log(data); // print the data
+//Undefined
+let data1 = undefined; //in this code data1 be undefined
+console.log(data1); // print the data1
---
+++ Datatypes.ts
@@ -0,0 +1,21 @@
+//Data Types
+
+//String
+let batsmanName:string="AB Devilliers";//This is the name of batsmanName
+console.log(batsmanName); // print the batsmanName
+
+//Number
+let score:number=200 //this is the score of batsman
+console.log(score) // print batsman score
+
+//Boolean
+let isout:boolean=true //this is the boolean value
+console.log(isout) // print the boolean value
+
+//Null
+let data:null=null; //in this code data be null
+console.log(data) // print the data
+
+//Undefined
+let data1:undefined = undefined; //in this code data1 be undefined
+console.log(data1); // print the data1
